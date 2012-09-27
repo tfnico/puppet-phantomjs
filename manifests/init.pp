646525908c74af4bcfc3e1f088e32d7e54c59343
Thomas Ferris Nicolaisen <thomas.nicolaisen@viaboxx.de>
2012-09-27 12:26:46 +0200

    Various fixes to install phantomjs 1.7.0 on debian
    - Fix download url
    - Upgrade to download phantomjs 1.7.0
    - Qualify path to system binaries
    - Comment out cyclic nuke-old-version dependency

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-class phantomjs($version = "1.5.0" ) {
+class phantomjs($version = "1.7.0" ) {
 
     if $::architecture == "amd64" or $::architecture == "x86_64" {
         $platid = "x86_64"
@@ -6,42 +6,44 @@ class phantomjs($version = "1.5.0" ) {
         $platid = "x86"
     }
 
-    $filename = "phantomjs-${version}-linux-${platid}-dynamic.tar.gz"
+    $suffix = "${version}-linux-${platid}"
+    $filename = "phantomjs-${suffix}.tar.bz2"
     $phantom_src_path = "/usr/local/src/phantomjs-${version}/"
     $phantom_bin_path = "/opt/phantomjs/"
 
     file { $phantom_src_path : ensure => directory }
 
-    exec { "download-${filename}" : 
-        command => "wget http://phantomjs.googlecode.com/files/${filename} -O ${filename}",
+    exec { "download-${filename}" :
+        command => "/usr/bin/wget https://phantomjs.googlecode.com/files/${filename} -O ${filename}",
         cwd => $phantom_src_path,
         creates => "${phantom_src_path}${filename}",
         require => File[$phantom_src_path]
     }
-    
+
     exec { "extract-${filename}" :
-        command     => "tar xvfz ${filename} -C /opt/",
-        creates     => "/opt/phantomjs/",
+        command     => "/bin/tar xvf ${filename} -C /opt/",
+        creates     => "/opt/phantomjs-${suffix}/",
         cwd         => $phantom_src_path,
         require     => Exec["download-${filename}"],
+    }
+
+    file { "/opt/phantomjs" :
+        target  => "/opt/phantomjs-${suffix}",
+        ensure  => link,
+        require => Exec["extract-${filename}"],
     }
 
     file { "/usr/local/bin/phantomjs" :
         target => "${phantom_bin_path}/bin/phantomjs",
         ensure => link,
-        require     => Exec["extract-${filename}"],
-    }
-    
-    file { "/usr/bin/phantomjs" :
-        target => "${phantom_bin_path}/bin/phantomjs",
-        ensure => link,
-        require     => Exec["extract-${filename}"],
-    }
-    
-    exec { "nuke-old-version-on-upgrade" :
-        command => "rm -Rf /opt/phantomjs /usr/local/bin/phantomjs",
-        unless => "test -f /usr/local/bin/phantomjs && /usr/local/bin/phantomjs --version | grep ${version}",
-        before => Exec["download-${filename}"]
+        require     => File["/opt/phantomjs"],
     }
 
+    exec { "nuke-old-version-on-upgrade" :
+        command => "/bin/rm -Rf /opt/phantomjs /usr/local/bin/phantomjs",
+        unless => "/usr/bin/test -f /usr/local/bin/phantomjs && /usr/local/bin/phantomjs --version | grep ${version}",
+        # Commented out because it causes a cyclic dependency
+        # before => Exec["download-${filename}"]
+    }
 }
+
